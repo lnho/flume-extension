4b9edc80b69ed593316f7dadcef8d195dd6c2bf1
yurun <miao.work2010@gmail.com>
2016-09-01 22:27:34 +0800

    dev

--- src/main/java/com/weibo/dip/flume/extension/source/MultithreadingKafkaSource.java
+++ src/main/java/com/weibo/dip/flume/extension/source/MultithreadingKafkaSource.java
@@ -148,7 +148,7 @@ public class MultithreadingKafkaSource extends AbstractSource implements EventDr
 					}
 				}
 
-				flush(events);
+				// flush(events);
 			} catch (Throwable e) {
 				LOGGER.error("KafkaConsumer error: " + ExceptionUtils.getFullStackTrace(e));
 			}
