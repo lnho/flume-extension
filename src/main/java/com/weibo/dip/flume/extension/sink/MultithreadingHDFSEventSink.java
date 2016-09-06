20aa4188aa56db2912d7709e38f2287b5be3c03e
yurun <miao.work2010@gmail.com>
2016-09-06 11:08:04 +0800

    dev

--- src/main/java/com/weibo/dip/flume/extension/sink/MultithreadingHDFSEventSink.java
+++ src/main/java/com/weibo/dip/flume/extension/sink/MultithreadingHDFSEventSink.java
@@ -3,12 +3,18 @@
  */
 package com.weibo.dip.flume.extension.sink;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.flume.Channel;
 import org.apache.flume.Context;
+import org.apache.flume.Event;
 import org.apache.flume.EventDeliveryException;
+import org.apache.flume.Transaction;
 import org.apache.flume.conf.Configurable;
 import org.apache.flume.sink.AbstractSink;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
  * @author yurun
@@ -16,25 +22,89 @@ import org.slf4j.LoggerFactory;
  */
 public class MultithreadingHDFSEventSink extends AbstractSink implements Configurable {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(MultithreadingHDFSEventSink.class);
+	// private static final Logger LOGGER =
+	// LoggerFactory.getLogger(MultithreadingHDFSEventSink.class);
 
-	@Override
-	public void configure(Context context) {
+	private int threads;
+
+	private ExecutorService sinkers;
+
+	private boolean sinkStoped;
+
+	private int batchSize;
+
+	private class Sinker implements Runnable {
+
+		@Override
+		public void run() {
+			while (!sinkStoped) {
+				Channel channel = getChannel();
+
+				Transaction transaction = channel.getTransaction();
+
+				transaction.begin();
+
+				try {
+					int txnEventCount = 0;
+
+					for (txnEventCount = 0; txnEventCount < batchSize; txnEventCount++) {
+						Event event = channel.take();
+						if (event == null) {
+							break;
+						}
+
+					}
+
+					transaction.commit();
+				} catch (Exception e) {
+					transaction.rollback();
+				} finally {
+					transaction.close();
+				}
+			}
+		}
 
 	}
 
 	@Override
+	public void configure(Context context) {
+		threads = context.getInteger("threads", 1);
+
+		sinkers = Executors.newFixedThreadPool(threads,
+				new ThreadFactoryBuilder().setNameFormat("hdfs-" + getName() + "-sink-sinker-%d").build());
+
+		batchSize = context.getInteger("batchSize", 1000);
+	}
+
+	@Override
 	public synchronized void start() {
+		sinkStoped = false;
+
+		for (int index = 0; index < threads; index++) {
+			sinkers.submit(new Sinker());
+		}
+
 		super.start();
 	}
 
 	@Override
 	public Status process() throws EventDeliveryException {
-		return null;
+		return Status.BACKOFF;
 	}
 
 	@Override
 	public synchronized void stop() {
+		sinkStoped = true;
+
+		sinkers.shutdown();
+
+		while (!sinkers.isTerminated()) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+			}
+		}
+
 		super.stop();
 	}
 
