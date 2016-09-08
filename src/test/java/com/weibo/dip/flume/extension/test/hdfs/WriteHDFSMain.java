81f86dc59f7d036ed49880920065282d96e9b09f
yurun <miao.work2010@gmail.com>
2016-09-08 10:17:32 +0800

    dev

--- src/test/java/com/weibo/dip/flume/extension/test/hdfs/WriteHDFSMain.java
+++ src/test/java/com/weibo/dip/flume/extension/test/hdfs/WriteHDFSMain.java
@@ -45,15 +45,13 @@ public class WriteHDFSMain {
 				String line = "_accesskey=sinaedgeahsolci14ydn&_ip=221.182.130.57&_port=80&_an=221.182.130.57&_data=d7.sina.com.cn 111.58.151.80 0 TCP_IMS_HIT [29/Aug/2016:00:59:42 +0800] \"GET /pfpghc2/201608/10/c2a4db4ca881411f86477f2febf4e201.jpg HTTP/0.0\" 304 0 \"http://news.sina.cn/sh?vt=4&pos=108\" \"-\" \"-\" \"Mozilla/5.0 (Linux; U; Android 4.3; zh-CN; HUAWEI C8816D Build/HuaweiC8816D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.9.10.788 U3/0.8.0 Mobile Safari/534.30\" *Not IP address [0]*";
 
 				while (!stop) {
-					writer.write(line);
-
-					// LOGGER.info("write: " + line);
-
 					try {
 						Thread.sleep(5000);
 					} catch (InterruptedException e) {
 					}
 				}
+
+				writer.write(line);
 
 				writer.close();
 
